9aeb55777c98e8b9d6cce8cc892f6a942bdfa4de
yaraallam <47528145+yaraallam@users.noreply.github.com>
2019-04-30 15:54:57 +0300

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PublicMenu from '../components/menus/publicMenu.js'
+import Header from '../components/banners/header1.js'
+import {Segment,Image} from "semantic-ui-react"
+
+export default class Categories extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentWillMount() {
+  }
+
+  render(){
+    return(
+      <div>
+        <Header title="Categories"/>
+        <PublicMenu location={this.props.location}/>
+        <div style={{padding:'4rem'}}>
+          <Segment loading>
+            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+          </Segment>
+        </div>
+      </div>
+    )
+	}
+}
